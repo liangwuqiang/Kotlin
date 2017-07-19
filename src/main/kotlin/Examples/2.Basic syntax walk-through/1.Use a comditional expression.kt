ae8df61e7d24be6399632f86eba5e5c0d623040e
liangwuqiang <l_wuqiang@126.com>
2017-07-19 17:58:16 +0800

    a

--- src/main/kotlin/Examples/2.Basic syntax walk-through/1.Use a comditional expression.kt
+++ src/main/kotlin/Examples/2.Basic syntax walk-through/1.Use a comditional expression.kt
@@ -1,7 +1,12 @@
+//要点：if表达式，从命令行读取的字符创需要.toInt()，单行函数的定义
+//简明扼要，不废话，去掉多余的东西
 /**
  * `if` is an expression, i.e. it returns a value.
+ * `if`是个表达式，例如，它能返回一个值。
  * Therefore there is no ternary operator (condition ? then : else),
+ * 所以Kotlin中没有三元操作符 (condition ? then : else),
  * because ordinary `if` works fine in this role.
+ * 因为普通的`if`能够很好地扮演这种角色。
  * See http://kotlinlang.org/docs/reference/control-flow.html#if-expression
  */
 fun main(args: Array<String>) {
