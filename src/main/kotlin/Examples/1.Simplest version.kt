67450b7d4c9e57b753e79d9998e1105392712852
liangwuqiang <l_wuqiang@126.com>
2017-07-19 16:24:08 +0800

    a

---
+++ src/main/kotlin/Examples/1.Simplest version.kt
@@ -0,0 +1,16 @@
+/**
+ * We declare a package-level function main which returns Unit
+ * 我们声明一个返回Unit的包级别的main函数，
+ * and takes an Array of strings as a parameter.
+ * 函数采用字符串数组作为参数。
+ * Note that semicolons are optional.
+ * 注意，分号是可选的。
+ */
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+    println("嗨，你好！")
+}
+
+//Hello, world!
+//嗨，你好！
